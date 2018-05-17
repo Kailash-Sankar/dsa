f14713ab098d278c45eb5b17221f6052d8a71b7e
Kailash-Sankar <Kailash.Sankar@outlook.com>
2018-05-17 08:49:45 +0530

    optimized comparison

--- Python/pairs.py
+++ Python/pairs.py
@@ -5,10 +5,10 @@ import numpy as np
 import time
 import random
 
-def minimum_distance(s):
-    #print('---')
-    #print(s)
-    
+# another optimized solution
+import ex_pairs
+
+def minimum_distance(s):    
     # calculate euclidean distance
     if len(s) <= 3:
        return naive(s)
@@ -18,36 +18,33 @@ def minimum_distance(s):
     dl = minimum_distance(s[:mid])
     dr = minimum_distance(s[mid:])
     
-    #print('-->',dl,dr);
     # minimum distance    
     d = min(dl,dr)
        
-    # find crossover distances
-    c = [];
-    #print('mid x',s[mid,0]);
-    for i in range(0,len(s)):
-        #print('--diffs',s[mid,0],s[i,0]);
-        if abs(s[mid,0] - s[i,0]) < d:
+    # points along mid 
+    c = [];    
+    for i in range(0,len(s)):        
+        if abs(s[mid,0] - s[i,0]) <= d:
             c.append(s[i]);
     
-    #print('==>',c,d);
-    if len(c) > 0:
+    # crossover distance
+    if len(c) > 1:
         d = strip_distance(np.array(c),d);        
-    #print('==>dx:',d);
     
     return d;
    
 
-def strip_distance(s,d):    
-    s = s[s[:,1].argsort()]
-    #print(" -- strip data --",s);
+def strip_distance(s,d):
+    # sort along y-axis
+    s = s[s[:,1].argsort()]    
     
+    # find minimum distance
     for i in range(0,len(s)):
-        j = i+1;
-        while j < len(s) and abs(s[i,1] - s[j,1]) < d:
-            dx = euclidean(s[i],s[j])
-            d= min(d,dx);
-            j+=1
+        lmt = min(i+7,len(s))
+        for j in range(i+1,min(i+8,len(s))):
+            if abs(s[i,1] - s[j,1]) <= d:
+                dx = euclidean(s[i],s[j])
+                d = min(d,dx);            
     return d;
 
 def naive(s):
@@ -57,28 +54,25 @@ def naive(s):
             d.append(euclidean(s[i],s[j]))            
     return min(d)
 
-def euclidean(p,q):    
-    diff = p-q
-    sqr = np.square(diff)    
-    pq = np.absolute(np.sum(sqr))
-    pqx = np.sqrt(pq)
-    #print('<>',p,q,diff,sqr,pq,pqx);
-    return pqx
+def euclidean(p,q):       
+    pq  = np.sum(np.square(p-q))
+    pqx = np.sqrt(pq)    
+    return pqx;
 
 def run_test():
-    x_limit = 10**1
-    y_limit = 10**9
-    data = [random.randrange(2,10**3)]
+    x_limit = 10**5
+    y_limit = 10**5
+    data = [random.randint(10,20)]
     
     print('==============================');
-    print('test size',data[0]);
+    print('Test size',data[0]);
     for i in range(0,data[0]):
-        data.append(random.randrange(-1*x_limit,x_limit))
-        data.append(random.randrange(-1*y_limit,y_limit))
+        data.append(random.randint(-1*x_limit,x_limit))
+        data.append(random.randint(-1*y_limit,y_limit))
+    #print('Test data',data);
     
     #custom tests
-    #data = [3,5000,500,1,5000,-100,-100];
-        
+    #data = [13, 9, -10, 2, 10, -8, 1, 8, 4, -10, 2, 2, 1, -4, 6, -4, -7, -1, -3, -6, 8, -9, 9, -4, -2, 5, -7]       
     s = extract_data(data);
     
     # naive
@@ -89,12 +83,19 @@ def run_test():
     # optimized
     s = s[s[:,0].argsort()]      
     md2 = minimum_distance(s)
-    opt_time = time.time()   
+    opt_time = time.time()
     
+    #optimized2    
+    x,y = s[:,0],s[:,1]    
+    md3 = ex_pairs.solution(x,y)
+    opt_time2 = time.time()
+    
+    # results
     print("Naive: {0:.9f}".format(md1),'time: ',naive_time - start_time)
-    print("Optimized: {0:.9f}".format(md2),'time: ',opt_time - naive_time)
+    print("Optz1: {0:.9f}".format(md2),'time: ',opt_time - naive_time)
+    print("Optz2: {0:.9f}".format(md3),'time: ',opt_time2 - opt_time)
     
-    return md2-md1;
+    return md2-md3;
 
 def extract_data(data):
     return np.array(list(zip(data[1::2],data[2::2])),dtype='int64')
@@ -106,10 +107,10 @@ def main():
     print("{0:.9f}".format(minimum_distance(s)))    
 
 if __name__ == '__main__':
-    for i in range(0,101):
+    for i in range(0,10):
         if run_test() !=0 :
             break;
-    print('Ran',i,'Tests');
+    print('Ran',i+1,'Tests');
     #data = main()
 
     
