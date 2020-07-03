15719862586a93678fd3b4cb0a52e8d3acb727bf
Kailash Sankar <Kailash.Sankar@outlook.com>
2020-07-03 19:48:41 +0530

    Create reverseInPlace.js

---
+++ Arrays/reverseInPlace.js
@@ -0,0 +1,13 @@
+ // reverse an array of characters in palce
+ 
+ // arrayOfChars.reverse() works
+ // this solution is generic to all languages
+ function reverse(arrayOfChars) {
+
+  // swap left and right, meet in the middle
+  for(let i=0, j=arrayOfChars.length; i<j; i++,j--) {
+    let temp = arrayOfChars[i];
+    arrayOfChars[i] = arrayOfChars[j];
+    arrayOfChars[j] = temp;
+  }  
+}
